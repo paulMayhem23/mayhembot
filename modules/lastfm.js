4a1c95dcece3e0b87b087dcd058c10cd86ad5508
paul mayhem <pmayhem23@gmail.com>
2012-07-09 03:59:14 +0100

    last minute stuff on a couple of custom modules

--- modules/lastfm.js
+++ modules/lastfm.js
@@ -1,7 +1,7 @@
 var customCommands = {
     name: 'lastfm', // command to display last fm url, or whatever you like
     command: function(options) {
-            options.cmbot.bot.speak("Our last fm page is here - http://www.last.fm/user/clubmayhem23");
+            options.cmbot.bot.speak("Our last fm page is here - http://www.last.fm/user/(your last fm)");
     },
     modonly: false,
     pmonly: false,
--- modules/rules.js
+++ modules/rules.js
@@ -1,7 +1,7 @@
 var customCommands = {
-    name: 'rules', // This is what the bot will respond to (ie, /beer)
+    name: 'rules', // rules of the room
     command: function(options) {
-            options.cmbot.bot.speak("Club Mayhem is a hangout for hackers, coders and lurkers of all kinds, play mostly what you want, nothing too commercial, no wubstep, respect the other users, excessive lamers and haters will be banned. for guidance on what we like to hear, check http://www.last.fm/user/clubmayhem23");
+            options.cmbot.bot.speak("these are the rules for this room");
     },
     modonly: false,
     pmonly: false,
